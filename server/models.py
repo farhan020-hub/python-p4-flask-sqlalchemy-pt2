58c2dd64cfa5bf777918a3857dddcca423e14517
Abdiaziz <abdiazizsaman394@gmail.com>
2024-01-10 10:45:58 +0300

    completed assignment

--- server/models.py
+++ server/models.py
@@ -1,11 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
-
-db = SQLAlchemy(metadata=metadata)
+db = SQLAlchemy()
 
 class Owner(db.Model):
     __tablename__ = 'owners'
@@ -20,7 +15,7 @@ class Owner(db.Model):
 
 class Pet(db.Model):
     __tablename__ = 'pets'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
     species = db.Column(db.String)
